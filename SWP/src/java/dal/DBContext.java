10151c8a4dd080ffa7d6b67f40158a92850aec82
maura2112 <no.72bylc@gmail.com>
2023-02-05 02:40:51 +0700

    product detail take1

--- SWP/src/java/dal/DBContext.java
+++ SWP/src/java/dal/DBContext.java
@@ -1,20 +1,21 @@
-
 package dal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DBContext {
+
     protected Connection connection;
-    public DBContext()
-    {
+
+    public DBContext() {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
             String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP";
-            String username = "sa";
-            String password = "linh3092";
+
+            String username = "truong";
+            String password = "1234";
+
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
